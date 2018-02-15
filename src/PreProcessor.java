ea9d31a4294bd4e4e0b50ec5ddd17fad9e97b138
Ian Douglas <iandouglas@lclark.edu>
2018-02-15 12:44:43 -0800

    working on crop image

--- src/PreProcessor.java
+++ src/PreProcessor.java
@@ -5,8 +5,20 @@ import javax.imageio.ImageIO;
 
 public class PreProcessor {
 
-    public static void crop(){
+    public static void main( String[] args){
+        crop();
+    }
 
+    public static void crop(){
+        try{
+            File f = new File("../assets/cafe.png");
+            BufferedImage image = ImageIO.read(f);
+            int height = image.getHeight();
+            int width = image.getWidth();
+            System.out.println("Height = " + height + "; Width = " + width);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
     }
 
 
