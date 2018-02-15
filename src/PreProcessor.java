6b978b7fa81ecb6941abe3ce8d0753f2df9d566d
Ian Douglas <iandouglas@lclark.edu>
2018-02-15 14:22:47 -0800

    Crop Image methods work correctly in PreProcessor Class

--- src/PreProcessor.java
+++ src/PreProcessor.java
@@ -2,25 +2,49 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.lang.Math;
 
 public class PreProcessor {
 
-    public static void main( String[] args){
-        crop();
+    public static void main(String[] args) {
+
     }
 
-    public static void crop(){
-        try{
-            File f = new File("../assets/cafe.png");
-            BufferedImage image = ImageIO.read(f);
-            int height = image.getHeight();
-            int width = image.getWidth();
-            System.out.println("Height = " + height + "; Width = " + width);
-        } catch (IOException ioe){
-            ioe.printStackTrace();
+    /**
+     * Crops all the images
+     * @param frames The total number of frames in the video to crop.
+     */
+    public static void crop(int[] point1, int[] point2, int frames) {
+        for (int i = 0; i < frames; i++) {
+            try {
+                BufferedImage image = ImageIO.read(new File("assets/" + i + ".png"));
+                BufferedImage subimage = cropImage(image,point1,point2);
+                ImageIO.write(subimage, "png", new File("assets/" + i + ".png"));
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
         }
     }
 
+    static int[] topLeft(int[] point1, int[]point2) {
+        int point[] = new int[2];
+        if (point1[0] <= point2[0]) {
+            point[0] = point1[0];
+        } else {
+            point[0] = point2[0];
+        }
+        if (point1[1] <= point2[1]) {
+            point[1] = point1[1];
+        } else {
+            point[1] = point2[1];
+        }
+        return point;
+    }
+
+    static BufferedImage cropImage(BufferedImage image, int[] point1, int[]point2) {
+        int[] topLeft = topLeft(point1, point2);
+        return image.getSubimage(topLeft[0], topLeft[1], Math.abs(point1[0] - point2[0]), Math.abs(point1[1] - point2[1]));
+    }
 
 
 }
