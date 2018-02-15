7c4329911a1f73fc0991a3a5f093b64795eff663
George Shoemaker <george@lclark.edu>
2018-02-15 15:07:27 -0800

    added example code from jcodec.org, Picture class is nowhere to be found

--- src/PreProcessor.java
+++ src/PreProcessor.java
@@ -1,11 +1,26 @@
+import org.jcodec.api.awt.AWTFrameGrab;
+import org.jcodec.scale.*;
+
+
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class PreProcessor {
 
-    public static void crop(){
+    public static void main(String args[]) {
+        int frameNumber = 1;
+        Picture picture = AWTFrameGrab.getFrameFromFile(new File("sample.mp4"), frameNumber);
+
+//for JDK (jcodec-javase)
+        BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+        ImageIO.write(bufferedImage, "png", new File("frame1.png"));
+    }
+
+
+    public static void crop() {
 
     }
 
