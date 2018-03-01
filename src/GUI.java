5b8af998af3e0403a9e7b93ca2f391833752d5c8
Ian Douglas <iandouglas@lclark.edu>
2018-03-01 14:14:45 -0800

    fixed GUI image display to properly update images

--- src/GUI.java
+++ src/GUI.java
@@ -1,30 +1,33 @@
 import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.awt.geom.*;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame {
     private int currentFrame;
     private JPanel buttonPanel;
     private JButton nextFrame;
     private JButton prevFrame;
     private JButton crop;
-    private double[] point1;
-    private double[] point2;
+    private int[] point1;
+    private int[] point2;
     public ImageComponent frame;
     private static final int DEFAULT_WIDTH = 100;
     private static final int DEFAULT_HEIGHT = 100;
 
-    public GUI(){
+    public GUI() {
         GridBagLayout layout = new GridBagLayout();
         setLayout(layout);
 
-        point1 = new double[2];
-        point2 = new double[2];
+        point1 = new int[2];
+        point2 = new int[2];
 
         //construct components
-        currentFrame = 0;
+        currentFrame = 1;
         //setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
         //make buttons for frames
@@ -40,7 +43,7 @@ public class GUI extends JFrame{
         buttonPanel.add(prevFrame);
         buttonPanel.add(crop);
         //add an image component and make it draw the first image
-        frame = new ImageComponent("pic0.png");
+        frame = new ImageComponent("assets/img001.png");
         frame.setBorder(BorderFactory.createEtchedBorder());
         //add the image component to the screen
 
@@ -67,9 +70,8 @@ public class GUI extends JFrame{
         crop.addActionListener(cropAction);
 
 
-
-        add(buttonPanel, new GBC(1,0).setFill(GBC.EAST).setWeight(100,0).setInsets(1));
-        add(frame, new GBC(2,0,1,4).setFill(GBC.BOTH).setWeight(500,500));
+        add(buttonPanel, new GBC(1, 0).setFill(GBC.EAST).setWeight(100, 0).setInsets(1));
+        add(frame, new GBC(2, 0, 1, 4).setFill(GBC.BOTH).setWeight(500, 500));
         pack();
     }
 
@@ -86,47 +88,44 @@ public class GUI extends JFrame{
 
     }
 
-    private class StepAction extends AbstractAction
-    {
+    private class StepAction extends AbstractAction {
         private int number;
         private ImageComponent ourFrame;
 
-        public StepAction(int direction)
-        {
+        public StepAction(int direction) {
             number = direction;
 
         }
 
-        public void actionPerformed(ActionEvent event)
-        {
-            String frameToDraw;
+        public void actionPerformed(ActionEvent event) {
             currentFrame += number;
-            frameToDraw = "pic" + Integer.toString(currentFrame) + ".png";
+            String frameToDraw = "assets/img" + String.format("%03d", currentFrame) + ".png";
             frame.setImage(frameToDraw);
+            revalidate();
             repaint();
         }
     }
 
-    private class CropAction implements ActionListener
-    {
+    private class CropAction implements ActionListener {
 
-        public CropAction(){
+        public CropAction() {
         }
 
         public void actionPerformed(ActionEvent event) {
 
-                point1[0] = frame.squares.get(0).getCenterX();
-                point1[1] = frame.squares.get(0).getCenterY();
+            point1[0] = (int) frame.squares.get(0).getCenterX();
+            point1[1] = (int) frame.squares.get(0).getCenterY();
 
-                point2[0] = frame.squares.get(1).getCenterX();
-                point2[1] = frame.squares.get(1).getCenterY();
+            point2[0] = (int) frame.squares.get(1).getCenterX();
+            point2[1] = (int) frame.squares.get(1).getCenterY();
 
-                //PreProcessor.crop(point1, point2, NUMBER_OF_FRAMES);
-                repaint();
+            PreProcessor.crop(point1, point2, 90);
+            revalidate();
+            repaint();
         }
     }
 
-    public void drawImage(String fileName){
+    public void drawImage(String fileName) {
         add(new ImageComponent(fileName));
         pack();
     }
@@ -146,8 +145,7 @@ class ImageComponent extends JComponent {
     private Rectangle2D current; // the square containing the mouse cursor
     private Image image;
 
-    public ImageComponent(String fileName)
-    {
+    public ImageComponent(String fileName) {
 
         image = new ImageIcon(fileName).getImage();
         squares = new ArrayList<>();
@@ -157,13 +155,17 @@ class ImageComponent extends JComponent {
         addMouseMotionListener(new MouseMotionHandler());
     }
 
-    public void setImage(String fileName){
-        image = new ImageIcon(fileName).getImage();
+    public void setImage(String fileName) {
+        try{
+            image = ImageIO.read(new File(fileName));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        //image = new ImageIcon(fileName).getImage();
 
     }
 
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         if (image == null) return;
 
         int imageWidth = image.getWidth(null);
@@ -180,93 +182,87 @@ class ImageComponent extends JComponent {
             g2.draw(r);
 
     }
-    public Dimension getPreferredSize() { return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT); }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 
 
-
-        /**
-         * Finds the first square containing a point.
-         * @param p a point
-         * @return the first square that contains p
-         */
-        public Rectangle2D find(Point2D p)
-        {
-            for (Rectangle2D r : squares)
-            {
-                if (r.contains(p)) return r;
-            }
-            return null;
+    /**
+     * Finds the first square containing a point.
+     *
+     * @param p a point
+     * @return the first square that contains p
+     */
+    public Rectangle2D find(Point2D p) {
+        for (Rectangle2D r : squares) {
+            if (r.contains(p)) return r;
         }
+        return null;
+    }
 
-        /**
-         * Adds a square to the collection.
-         * @param p the center of the square
-         */
-        public void add(Point2D p)
-        {
-            double x = p.getX();
-            double y = p.getY();
+    /**
+     * Adds a square to the collection.
+     *
+     * @param p the center of the square
+     */
+    public void add(Point2D p) {
+        double x = p.getX();
+        double y = p.getY();
 
-            current = new Rectangle2D.Double(x - SIDELENGTH / 2, y - SIDELENGTH / 2, SIDELENGTH,
-                    SIDELENGTH);
-            squares.add(current);
-            repaint();
-        }
+        current = new Rectangle2D.Double(x - SIDELENGTH / 2, y - SIDELENGTH / 2, SIDELENGTH,
+                SIDELENGTH);
+        squares.add(current);
+        repaint();
+    }
 
-        /**
-         * Removes a square from the collection.
-         * @param s the square to remove
-         */
-        public void remove(Rectangle2D s)
-        {
-            if (s == null) return;
-            if (s == current) current = null;
-            squares.remove(s);
-            repaint();
-        }
+    /**
+     * Removes a square from the collection.
+     *
+     * @param s the square to remove
+     */
+    public void remove(Rectangle2D s) {
+        if (s == null) return;
+        if (s == current) current = null;
+        squares.remove(s);
+        repaint();
+    }
 
-        private class MouseHandler extends MouseAdapter
-        {
-            public void mousePressed(MouseEvent event)
-            {
-                // add a new square if the cursor isn't inside a square
-                current = find(event.getPoint());
-                if (squares.size() < 2) {
-                    if (current == null) add(event.getPoint());
-                }
-            }
-
-            public void mouseClicked(MouseEvent event)
-            {
-                // remove the current square if double clicked
-                current = find(event.getPoint());
-                if (current != null && event.getClickCount() >= 2) remove(current);
+    private class MouseHandler extends MouseAdapter {
+        public void mousePressed(MouseEvent event) {
+            // add a new square if the cursor isn't inside a square
+            current = find(event.getPoint());
+            if (squares.size() < 2) {
+                if (current == null) add(event.getPoint());
             }
         }
 
-        private class MouseMotionHandler implements MouseMotionListener
-        {
-            public void mouseMoved(MouseEvent event)
-            {
-                // set the mouse cursor to cross hairs if it is inside
-                // a rectangle
+        public void mouseClicked(MouseEvent event) {
+            // remove the current square if double clicked
+            current = find(event.getPoint());
+            if (current != null && event.getClickCount() >= 2) remove(current);
+        }
+    }
 
-                if (find(event.getPoint()) == null) setCursor(Cursor.getDefaultCursor());
-                else setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-            }
+    private class MouseMotionHandler implements MouseMotionListener {
+        public void mouseMoved(MouseEvent event) {
+            // set the mouse cursor to cross hairs if it is inside
+            // a rectangle
 
-            public void mouseDragged(MouseEvent event)
-            {
-                if (current != null)
-                {
-                    int x = event.getX();
-                    int y = event.getY();
+            if (find(event.getPoint()) == null) setCursor(Cursor.getDefaultCursor());
+            else setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        }
 
-                    // drag the current rectangle to center it at (x, y)
-                    current.setFrame(x - SIDELENGTH / 2, y - SIDELENGTH / 2, SIDELENGTH, SIDELENGTH);
-                    repaint();
-                }
+        public void mouseDragged(MouseEvent event) {
+            if (current != null) {
+                int x = event.getX();
+                int y = event.getY();
+
+                // drag the current rectangle to center it at (x, y)
+                current.setFrame(x - SIDELENGTH / 2, y - SIDELENGTH / 2, SIDELENGTH, SIDELENGTH);
+                repaint();
             }
         }
     }
+}
 
--- src/PreProcessor.java
+++ src/PreProcessor.java
@@ -15,6 +15,7 @@ public class PreProcessor {
 
     /**
      * Crops all the images
+     *
      * @param point1 An integer array {x,y} of one coordinate to crop.
      * @param point2 An integer array {x,y} of one coordinate to crop.
      * @param frames The total number of frames in the video to crop.
@@ -22,9 +23,9 @@ public class PreProcessor {
     public static void crop(int[] point1, int[] point2, int frames) {
         for (int i = 0; i < frames; i++) {
             try {
-                BufferedImage image = ImageIO.read(new File("assets/img" + String.format("%03d", i) + ".png"));
-                BufferedImage subimage = cropImage(image,point1,point2);
-                ImageIO.write(subimage, "png", new File("assets/img" + String.format("%03d", i) + ".png"));
+                BufferedImage image = ImageIO.read(new File("assets/img" + String.format("%03d", i+1) + ".png"));
+                BufferedImage subimage = cropImage(image, point1, point2);
+                ImageIO.write(subimage, "png", new File("assets/img" + String.format("%03d", i+1) + ".png"));
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
@@ -34,7 +35,7 @@ public class PreProcessor {
     /**
      * @return An integer array {x,y} of the top left point, given any two points.
      */
-    static int[] topLeft(int[] point1, int[]point2) {
+    static int[] topLeft(int[] point1, int[] point2) {
         int point[] = new int[2];
         if (point1[0] <= point2[0]) {
             point[0] = point1[0];
@@ -51,14 +52,19 @@ public class PreProcessor {
 
     /**
      * Crops a given image.
+     *
      * @param image The image to crop.
      * @return A cropped image.
      */
-    static BufferedImage cropImage(BufferedImage image, int[] point1, int[]point2) {
+    static BufferedImage cropImage(BufferedImage image, int[] point1, int[] point2) {
         int[] topLeft = topLeft(point1, point2);
         return image.getSubimage(topLeft[0], topLeft[1], Math.abs(point1[0] - point2[0]), Math.abs(point1[1] - point2[1]));
     }
 
+    /**
+     *
+     * @param frames
+     */
     static void colorCorrectFrames(int frames) {
         for (int i = 0; i < frames; i++) {
             try {
@@ -72,30 +78,30 @@ public class PreProcessor {
     }
 
     static BufferedImage colorCorrect(BufferedImage image) {
-       int height =  image.getHeight();
-        int width =  image.getWidth();
-    LimitedQueue queue = new LimitedQueue(200);
+        int height = image.getHeight();
+        int width = image.getWidth();
+        LimitedQueue queue = new LimitedQueue(200);
 
         double threshold = 0;
         for (int y = 0; y < height; y++) {
             int average = 0;
             for (int x = 0; x < width; x++) {
-                int colorValue = image.getRGB(x,y);
+                int colorValue = image.getRGB(x, y);
                 //System.out.println("x: " +x+ " y: " + y + "Color: " + colorValue);
                 //get average RGB value
                 int red = (colorValue >> 16) & 0xFF;
-                int green = (colorValue>>8) & 0xFF;
+                int green = (colorValue >> 8) & 0xFF;
                 int blue = (colorValue) & 0xFF;
                 //System.out.println("Red " + red + " blue: " + blue + " green " + green);
-                int colorAvg = (red + green + blue)/ 3;
+                int colorAvg = (red + green + blue) / 3;
                 average += colorAvg;
                 if (colorAvg > threshold) {
-                    image.setRGB(x, y,Color.WHITE.getRGB());
+                    image.setRGB(x, y, Color.WHITE.getRGB());
                 } else {
-                    image.setRGB(x,y,Color.BLACK.getRGB());
+                    image.setRGB(x, y, Color.BLACK.getRGB());
                 }
             }
-            average = average/width;
+            average = average / width;
             queue.add(average);
             threshold = (queue.sum() * .75);
         }
@@ -114,13 +120,16 @@ class LimitedQueue extends LinkedList<Integer> {
     @Override
     public boolean add(Integer o) {
         super.add(o);
-        while (size() > limit) { super.remove(); }
+        while (size() > limit) {
+            super.remove();
+        }
         return true;
     }
+
     public int sum() {
         int ans = 0;
-        for(int i = 0; i < this.size(); i++){
-            ans+= this.get(i);
+        for (int i = 0; i < this.size(); i++) {
+            ans += this.get(i);
         }
         return ans / this.size();
     }
