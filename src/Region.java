d147f1118e1ac128d55f29974069aae6f5f92236
Ian Douglas <iandouglas@lclark.edu>
2018-03-20 14:03:00 -0700

    'region test'

---
+++ src/Region.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+
+public class Region {
+    private BufferedImage image;
+    private int avgValue;
+    private int depth;
+    private int width;
+    private int height;
+
+    public Region(Region Parent, BufferedImage image, int depth) {
+        this.image = image;
+        width = image.getWidth();
+        height = image.getHeight();
+        findAvgValue();
+    }
+
+    private void findAvgValue() {
+        int avg = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int colorValue = image.getRGB(i, j);
+                int r = (colorValue >> 16) & 0xFF;
+                int g = (colorValue >> 8) & 0xFF;
+                int b = (colorValue) & 0xFF;
+                avg += (r + g + b) / 3;
+            }
+        }
+        avgValue = avg / (width * height);
+    }
+
+    public int getAvgValue() {
+        return avgValue;
+    }
+
+
+}
