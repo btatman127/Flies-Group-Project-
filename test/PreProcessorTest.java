8c9d42ca53e6c75fe57112c9f411cab4c1529215
Simon Buan <simonbuan@gmail.com>
2021-02-11 15:23:37 -0800

    Remove unused imports from PreProcessorTest

--- test/PreProcessorTest.java
+++ test/PreProcessorTest.java
@@ -3,11 +3,8 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
