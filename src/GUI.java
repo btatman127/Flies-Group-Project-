e11c5683590db5875885135dbf7be59ff006f01a
Simon Buan <simonbuan@gmail.com>
2021-04-01 14:33:36 -0700

    Refactor two currentFrame variables into a single one

--- src/GUI.java
+++ src/GUI.java
@@ -24,7 +24,6 @@ public class GUI extends JFrame {
     private final static double ZONE_RADIUS = 4.5;
     private final static int MAX_LARVA = 5;
 
-    private int currentFrame;
     private File originalMovie;
     private Video movie;
     private int tempLarvaIndex;
@@ -143,8 +142,6 @@ public class GUI extends JFrame {
 
         history = new Stack<>();
 
-        //construct components
-        currentFrame = 0;
         tempLarvaIndex = -1;
 
         cropProgress = new JProgressBar();
@@ -412,13 +409,13 @@ public class GUI extends JFrame {
         frame.squares = new ArrayList<>();
 
         frame.displayPaths = false;
-        displayFrameNum.setText("Frame " + currentFrame + " of " + movie.getNumImages());
+        displayFrameNum.setText("Frame " + frame.currentFrame + " of " + movie.getNumImages());
         displayFrameNum.setEditable(false);
         displayFrameNum.setVisible(true);
 
         setButtonStates(ProgramState.PRE_CROP);
         pack();
-        frame.setImage(movie.getPathToFrame(currentFrame));
+        frame.setImage(movie.getPathToFrame(frame.currentFrame));
         validate();
         repaint();
     }
@@ -475,8 +472,7 @@ public class GUI extends JFrame {
         }
 
         originalMovie = video;
-        currentFrame = 1;
-        frame.currentFrame = 0;
+        frame.currentFrame = 1;
 
         openMovieDurationDialog();
     }
@@ -518,12 +514,11 @@ public class GUI extends JFrame {
         public void actionPerformed(ActionEvent event) {
             if (!changeFrameEnabled) return;
 
-            if (currentFrame + number >= 0 && currentFrame + number < movie.getNumImages()) {
-                currentFrame += number;
-                frame.currentFrame = currentFrame;
-                frame.setImage(movie.getPathToFrame(currentFrame + 1));
+            if (frame.currentFrame + number >= 0 && frame.currentFrame + number < movie.getNumImages()) {
+                frame.currentFrame += number;
+                frame.setImage(movie.getPathToFrame(frame.currentFrame + 1));
 
-                displayFrameNum.setText("Frame " + (currentFrame + 1) + " of " + movie.getNumImages());
+                displayFrameNum.setText("Frame " + (frame.currentFrame + 1) + " of " + movie.getNumImages());
                 pack();
                 revalidate();
                 repaint();
@@ -598,9 +593,9 @@ public class GUI extends JFrame {
                 System.out.println("Need to have 2 squares to crop the image.");
             }
 
-            currentFrame = 1;
-            frame.setImage(movie.getPathToFrame(currentFrame));
-            displayFrameNum.setText("Frame " + currentFrame + " of " + movie.getNumImages());
+            frame.currentFrame = 1;
+            frame.setImage(movie.getPathToFrame(frame.currentFrame));
+            displayFrameNum.setText("Frame " + frame.currentFrame + " of " + movie.getNumImages());
             revalidate();
             repaint();
 
@@ -619,9 +614,9 @@ public class GUI extends JFrame {
         }
 
         public void actionPerformed(ActionEvent event) {
-            currentFrame = 0;
-            frame.setImage(movie.getPathToFrame(currentFrame + 1));
-            displayFrameNum.setText("Frame " + (currentFrame + 1) + " of " + movie.getNumImages());
+            frame.currentFrame = 0;
+            frame.setImage(movie.getPathToFrame(frame.currentFrame + 1));
+            displayFrameNum.setText("Frame " + (frame.currentFrame + 1) + " of " + movie.getNumImages());
             frame.maxSquares = 5;
             setButtonStates(ProgramState.SELECTING_LARVAE);
             pack();
@@ -732,10 +727,10 @@ public class GUI extends JFrame {
                     pt[0] = (frame.squares.get(0).getCenterX() * xratio);
                     pt[1] = (frame.squares.get(0).getCenterY() * yratio);
 
-                    movie.retrackLarvaPosition(currentFrame, gui.getTempLarvaIndex(), pt);
+                    movie.retrackLarvaPosition(frame.currentFrame, gui.getTempLarvaIndex(), pt);
                     frame.remove(frame.squares.get(0));
                     frame.maxSquares = 0;
-                    movie.retrackLarvaPosition(currentFrame, gui.getTempLarvaIndex(), pt);
+                    movie.retrackLarvaPosition(frame.currentFrame, gui.getTempLarvaIndex(), pt);
                 }
 
                 setButtonStates(ProgramState.TRACKING);
@@ -793,7 +788,7 @@ public class GUI extends JFrame {
 
         public void actionPerformed(ActionEvent event) {
             FileDialog fd = new FileDialog(GUI.this, "Select where to save image", FileDialog.SAVE);
-            String defaultName = movie.getOriginalMovieName() + ".frame_" + (currentFrame + 1) + ".png";
+            String defaultName = movie.getOriginalMovieName() + ".frame_" + (frame.currentFrame + 1) + ".png";
             fd.setFile(defaultName);
             fd.setVisible(true);
             BufferedImage bi = new BufferedImage(frame.getImage().getWidth(null),
